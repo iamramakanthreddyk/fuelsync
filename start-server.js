8e06a7c862b047953766eca79cc106d573ee91d5
r.kowdamapalli <r.kowdampalli@accenture.com>
2025-06-23 21:13:17 +0200

    fix typescript dependencies

--- start-server.js
+++
@@ -1,8 +0,0 @@
-const { createApp } = require('./dist/app');
-
-const app = createApp();
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
